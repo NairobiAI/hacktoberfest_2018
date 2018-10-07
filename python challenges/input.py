e716da514672ba8c3793c392feceeae873195f2b
Alexandre Plateau <lexplt.dev@gmail.com>
2018-10-07 18:06:00 +0200

    completing input challenge

--- python challenges/input.py
+++ python challenges/input.py
@@ -1,2 +1,10 @@
 # Create a function that takes in a user's input and displays it
 # back to the screen
+
+def display_input():
+    text = input("Hey ! Write something ! ")
+    print(text)
+
+
+if __name__ == '__main__':
+    display_input()
