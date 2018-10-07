ebbf7cee879496b599069a7a3567e50dda8750eb
Alexandre Plateau <lexplt.dev@gmail.com>
2018-10-07 18:06:54 +0200

    completing list_comprehension challenge

--- python challenges/list_comprehension.py
+++ python challenges/list_comprehension.py
@@ -1,2 +1,6 @@
 # Create a list comprehension that returns a list of even numbers
 # from 1 - 20 (20 included)
+
+if __name__ == '__main__':
+    my_list = [i for i in range(0, 21) if i % 2 == 0]
+    print(my_list)
