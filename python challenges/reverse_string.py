db3d4d81a86513cbac5b37a63d56fb68c0b2dcf7
Alexandre Plateau <lexplt.dev@gmail.com>
2018-10-07 18:09:14 +0200

    completing reverse string challenge !

--- python challenges/reverse_string.py
+++ python challenges/reverse_string.py
@@ -1,2 +1,13 @@
 # Create a function takes in a string and returns
 # the reverse of that string
+
+def reverse(string):
+    output = ""
+    for c in string:
+        output = c + output
+    return output
+
+
+if __name__ == '__main__':
+    s = "hello world"
+    print("{} => {}".format(s, reverse(s)))
