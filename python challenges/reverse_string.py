ad61d0b9498a06baaf495530ad0cec2cc795402d
Shuayb S <shuayb01@users.noreply.github.com>
2018-10-12 23:01:09 -0400

    Completed String Reversal

--- python challenges/reverse_string.py
+++ python challenges/reverse_string.py
@@ -1,2 +1,10 @@
 # Create a function takes in a string and returns
 # the reverse of that string
+
+def stringReversal(str):
+    chars = list(str)
+    for i in range(len(str) // 2):
+        temp = chars[i]
+        chars[i] = chars[len(str) - i - 1]
+        chars[len(s) - i - 1] = temp
+    return ''.join(chars)
