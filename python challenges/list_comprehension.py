ab04ea83f1aabde7b9a5e1b6ca3be7d47b449ea8
kateshiru <catherinemunene802@gmail.com>
2018-10-17 13:40:40 +0300

    Finished list_comprehension challenge

--- python challenges/list_comprehension.py
+++ python challenges/list_comprehension.py
@@ -1,2 +1,14 @@
 # Create a list comprehension that returns a list of even numbers
 # from 1 - 20 (20 included)
+
+# function to include end number in range
+def rangeToEnd(start,end):
+	return range(start, end+1)
+
+#list comprehension
+evenNumberList = [x for x in rangeToEnd(1,20) if x % 2 == 0]
+
+#print even numbers
+print(evenNumberList)
+
+
